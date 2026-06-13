130d452faf366250d1b0a52a85632e742cd45a9b
specforceblack <specforceblack@gmail.com>
2026-06-13 18:06:17 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+  SPOTIFY_CLIENT_ID: '8841761bde0149d88d3223951cc27cb9',
+  SPOTIFY_REDIRECT_URI: 'https://specforceblack.github.io/fretboard',
+  TICKETMASTER_KEY: 'FggeVIGd5V9q9NQfVomiTLjbzxGablje',
+  GENIUS_KEY: 'your-genius-token-here',
+  REVERB_KEY: '0c3c60545ce0bf7a0a195c89a52562bb2287ee853104f8b0eee10632310b8306'
+}
